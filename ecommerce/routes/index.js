670c235cae3f2ca6a674d29c55743336794b36ce
Christopher Camacho <christophercamacho@Christophers-MacBook-Air.local>
2019-09-25 18:38:08 -0400

    Added signup template and csrf protection to all routes

--- ecommerce/routes/index.js
+++ ecommerce/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection); //all routes in this router, should be protected by csrfProtection
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +16,10 @@ router.get('/', function(req, res, next) {
     };
     res.render('shop/index', { title: 'E-commerce Website', products: productRows });
   }); // queries DB for Product collection, mongoose object
+});
+
+router.get('/user/signup', function(req, res, next) {
+  res.render('user/signup', {csrfToken: req.csrfToken()});
 });
 
 module.exports = router;
