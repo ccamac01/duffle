b0ce855c8b81c69acd377a8d4d6b082307d072e3
Christopher Camacho <christophercamacho@Christophers-MacBook-Air.local>
2019-10-12 17:31:08 -0400

    Added shopping-cart view page (need to fix dropdown menu per item)

--- ecommerce/routes/index.js
+++ ecommerce/routes/index.js
@@ -30,9 +30,14 @@ router.get('/add-to-cart/:id', (req, res, next) => {
     console.log(req.session.cart);
     res.redirect('/'); // redirect to product page
   });
+});
 
-
-
+router.get('/shopping-cart', (req, res, next) => {
+  if (!req.session.cart) {
+    return res.render('shop/cart', {products: null});
+  };
+  var cart = new Cart(req.session.cart);
+  res.render('shop/cart', {products: cart.itemList(), totalPrice: cart.totalPrice})
 });
 
 module.exports = router;
